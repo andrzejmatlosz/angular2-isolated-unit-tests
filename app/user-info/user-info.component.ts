be27eae5f012d2e2a38f5e277a0fb6445c40d9e8
andrzej <andrzej.matlosz@gmail.com>
2018-01-24 08:21:35 +0100

    Add very simple login logic

--- app/user-info/user-info.component.ts
+++ app/user-info/user-info.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-user-info',
     template: `
-        <div class="user-info">
-            {{user.name}}
+        <div class="user-info" *ngIf="user">
+            {{user?.name}}
+            <button (click)="logout()">Logout</button>
+        </div>
+        <div class="user-info" *ngIf="!user">
+            <button (click)="loginAsAdmin()">Admin</button>
+            <button (click)="loginAsUser()">User</button>
         </div>
     `,
 })
@@ -13,6 +18,17 @@ export class UserInfoComponent  {
     user: any;
 
     constructor(private userService: UserService) {
-        this.user = userService.user;
+    }
+
+    loginAsAdmin() {
+        this.user = this.userService.loginAsAdmin();
+    }
+
+    loginAsUser() {
+        this.user = this.userService.loginAsUser();
+    }
+
+    logout() {
+        this.user = null;
     }
 }
--- app/user-info/user.service.ts
+++ app/user-info/user.service.ts
@@ -1,8 +1,22 @@
+import { userInfo } from 'os';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UserService {
     user: any = {
-        name: 'Test user'
+        name: 'Test user',
+        isAdmin: false
     };
+
+    loginAsUser() {
+        this.user.isAdmin = false;
+        this.user.name = 'User';
+        return this.user;
+    }
+
+    loginAsAdmin() {
+        this.user.isAdmin = true;
+        this.user.name = 'Admin';
+        return this.user;
+    }
 }
